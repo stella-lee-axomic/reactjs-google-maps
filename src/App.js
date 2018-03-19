6cb22af351f77c44ad9d3e3c6d34dadf7ec3d194
stellalee <stella.lee@axomic.com>
2018-03-19 16:21:05 +0000

    reverse geocoding v1.0

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line
 import React from 'react';
 
 import './index.css';
@@ -9,16 +11,21 @@ export default class App extends React.Component {
         this.state = {
             zoom: 4,
             maptype: 'roadmap',
-            place_formatted: '',
+            formatted_address: '',
+            reverseGeocoding: '',
             title:'',
             place_id: '',
             lat: '',
             lng: ''
         };
-        // this.displayPosition = this.displayPosition.bind(this);
     }
 
     componentDidMount() {
+
+        const data = {
+            keyword: 'London',
+            projectTitle: 'My Project'
+        }
 
         // map & marker --------------------------------
         // let map = new window.google.maps.Map(document.getElementById('map'), {
@@ -57,11 +64,17 @@ export default class App extends React.Component {
         var searchBox = new window.google.maps.places.SearchBox(
             /** @type {HTMLInputElement} */(input));
 
+        // var geocoder = new window.google.maps.Geocoder;// undefine
+        // console.log(geocoder);
 
+
+
+
+        const component = this
 
         window.google.maps.event.addListener(searchBox, 'places_changed', function () {
             var places = searchBox.getPlaces();
-            if (places.length === 0) return;
+            if (places.length === 0) return; // length: always 1
 
             for (var i = 0, marker; marker = markers[i]; i++) {
                 marker.setMap(null);
@@ -69,6 +82,8 @@ export default class App extends React.Component {
             markers = [];
             var bounds = new window.google.maps.LatLngBounds();
             for (var i = 0, place; place = places[i]; i++) {
+
+
                 var image = {
                     url: place.icon,
                     size: new window.google.maps.Size(71, 71),
@@ -79,191 +94,102 @@ export default class App extends React.Component {
                 var marker = new window.google.maps.Marker({
                     draggable: true,
                     map: map,
-                    icon: image,
                     title: place.name,
                     position: place.geometry.location
                 });
                 // drag response
-                window.google.maps.event.addListener(marker, 'dragend', function(){
-                    displayPosition(this.getPosition());
+                window.google.maps.event.addListener(marker, 'dragend', function () {
+                    component.displayPosition(this.getPosition());
                 });
                 // click response
-                window.google.maps.event.addListener(marker, 'click', function(){
-                    displayPosition(this.getPosition());
+                window.google.maps.event.addListener(marker, 'click', function () {
+                    component.displayPosition(this.getPosition());
                 });
                 markers.push(marker);
-                bounds.extend(place.geometry.location);
-                // this.setState({
-                //     title: markers.title
-                // }).bind(this);
-                console.log('place name:', marker.title);
-                // address.push(places[0].formatted_address);
-                console.log('place address', places[0].formatted_address);
+
+
+                console.log('ADDING PLACE', place.formatted_address)
+                const { location } = place.geometry;
+
+                bounds.extend(location);
+                component.displayPosition(location);
+                component.displayAddress(place.formatted_address);
+                // component.geocodeLatLng()
             }
+
             map.fitBounds(bounds);
             map.setZoom(18);
         });
-        /////////////////
 
         window.google.maps.event.addListener(map, 'bounds_changed', function () {
             var bounds = map.getBounds();
             searchBox.setBounds(bounds);
         });
 
-        ////////////// displays a position on two <input> elements
-        function displayPosition (pos) {
-            // this.refs.lat.value = pos.lat();
-            document.getElementsByClassName('lat').value = pos.lat();
-            document.getElementsByClassName('lng').value = pos.lng();
-            this.setState({
-                lat: pos.lat(),
-                lng: pos.lng()
-            })
-            console.log(pos);
-            console.log('lat', pos.lat());
-            console.log('lng', pos.lng());
-        }
-
-
-       // displayPosition = (pos) => {
-       //      // this.refs.lat.value = pos.lat();
-       //      // document.getElementsByClassName('lat').value = pos.lat();
-       //      // document.getElementsByClassName('lng').value = pos.lng();
-       //      this.setState({
-       //          lat: pos.lat(),
-       //          lng: pos.lng()
-       //      });
-       //      // console.log(pos);
-       //      // console.log('lat', pos.lat());
-       //      // console.log('lng', pos.lng());
-       //  }
-
-        // map.addListener('zoom_changed', () => { // double click
-        //     this.setState({
-        //         zoom: map.getZoom(),
-        //     });
-        // });
-        // map.addListener('maptypeid_changed', () => {
-        //     this.setState({
-        //         maptype: map.getMapTypeId(),
-        //     });
-        // });
-        //
-        //
-        // // click to drop the pin
-        // new window.google.maps.event.addListener(map, 'click', function (event) {
-        //     placeMarker(event.latLng);
-        //     // console.log(event.latLng);
-        // });
-        //
-        // function placeMarker(location) {
-        //     var marker = new window.google.maps.Marker({
-        //         position: location,
-        //         map: map
-        //     });
-        // }
-        //
-        //
-        // // searchbox ------------------------------
-        // var input = document.getElementById('pac-input');
-        // map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(input);
-        // var places = [];
-        // var searchBox = new window.google.maps.places.SearchBox(input);
-        // searchBox.addListener('places_changed', function () {
-        //     places = searchBox.getPlaces(); // all the place info, address
-        //     if (places.length === 0) return;
-        // });
-        // markers.forEach(function (marker) {
-        //     marker.setMap(null);
-        // });
-        //
-        //
-        // // initialize the autocomplete functionality using the #pac-input input box
-        // let inputNode = document.getElementById('pac-input');
-        // map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(inputNode);
-        // let autoComplete = new window.google.maps.places.Autocomplete(inputNode);
-        // // var geocoder = new window.google.maps.Geocoder();
-        //
-        //
-        // autoComplete.addListener('place_changed', () => {
-        //     let place = autoComplete.getPlace();
-        //     if (!place.place_id) {
-        //         console.log('inside the if')
-        //     } else {
-        //         let location = place.geometry.location;
-        //         console.log(location);
-        //
-        //         this.setState({
-        //             place_formatted: place.formatted_address,
-        //             place_id: place.place_id,
-        //             place_location: location.toString(),
-        //         });
-        //         // bring the selected place in view on the map
-        //         map.fitBounds(place.geometry.viewport);
-        //         map.setCenter(location);
-        //
-        //         //drag and read latLng
-        //         // placeMarker(location);
-        //
-        //         marker.setPlace({
-        //             placeId: place.place_id,
-        //             location: location,
-        //         });
-        //     }
-        // });
-        // this.setState({
-        //     map: map
-        // });
     }
 
-    // handleChange = (evt) => {
-    //     let value = evt.target.value;
-    //     this.setState({
-    //         address: value
-    //     })
-    // }
+    displayPosition = (pos) => {
+        const {lat, lng} = pos
+
+        this.setState({
+            lat: lat(),
+            lng: lng()
+        })
+    }
+
+    displayAddress = (adr) => {
+        this.setState({
+            formatted_address: adr
+        })
+    }
+
+
+    // geocodeLatLng = (geocoder, map, infowindow) => {
+    //     // var input = document.getElementById('latlng').value;
+    //     // var latlngStr = input.split(',', 2);
+    //     var latlng = {lat: this.state.lat, lng: this.state.lng};
     //
-    // geocodeAddress() {
-    //     var map = this.state.map;
-    //     var address = this.state.address;
-    //     // console.log(this.state);// not coming in
-    //     var geocoder = new window.google.maps.Geocoder();
-    //     geocoder.geocode({'address': address}, function (results, status) {
+    //     // console.log(geocoder);
+    //
+    //     geocoder.geocode({'location': latlng}, function(results, status) {
     //         if (status === 'OK') {
-    //             map.setCenter(results[0].geometry.location);
-    //             var marker = new window.google.maps.Marker({
-    //                 map: map,
-    //                 draggable: true,
-    //                 animation: window.google.maps.Animation.DROP,
-    //                 position: results[0].geometry.location
-    //             });
+    //             if (results[0]) {
+    //                 console.log(results[0]);
+    //                 // map.setZoom(11);
+    //                 // var marker = new google.maps.Marker({
+    //                 //     position: latlng,
+    //                 //     map: map
+    //                 // });
+    //
+    //                 this.setState({
+    //                     reverseGeocoding: results[0].formatted_address
+    //                 })
+    //                 // infowindow.setContent(results[0].formatted_address);
+    //                 // infowindow.open(map, marker);
+    //             } else {
+    //                 window.alert('No results found');
+    //             }
     //         } else {
-    //             alert('Geocode was not successful for the following reason: ' + status); // invalid request
+    //             window.alert('Geocoder failed due to: ' + status);
     //         }
     //     });
     // }
-    //
-    // handleSubmit() {
-    //     this.geocodeAddress();
-    // }
+
 
     render() {
+        const { lat, lng, formatted_address, reverseGeocoding } = this.state;
+
         return (
             <div id='app'>
                 <div id='search'>
                     <input className="address" id='pac-input' type="textbox" />
-                        {/*// onChange={evt => this.handleChange(evt)}*/}
                 </div>
-
-                {/*<div>location: { this.state.address} </div>*/}
-                <div>address: {} </div>
-                <div ref='lat'>lat: {this.state.lat}</div>
-                <div ref='lng'>lng: {this.state.maptype}</div>
-
+                <div ref='lat'>lat: {lat}</div>
+                <div ref='lng'>lng: {lng}</div>
+                <div ref='lng'>{formatted_address} <button>save</button></div>
+                <div ref='lng'>{reverseGeocoding}</div>
                     <div id='map'>
                     </div>
-                    {/*<div id='block1'>hi*/}
-                    {/*</div>*/}
             </div>
         );
     }
